35cb017860bb602a9e0e177ba72de0d1bccb98ec
Sushant Pathania <SPathania@XEBIAINDIA.LOCAL>
2020-09-22 16:57:05 +0530

    initial

---
+++ src/Components/header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./styles.css";
+const Header = (props) => {
+    return ( 
+    <header> 
+        SushantTuT
+    </header>
+    )
+};
+
+export default Header;
